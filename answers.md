23f220476ceb03c84ac7b6866e31437a3b39992e
caitlyngif <caitlyngif@gmail.com>
2025-10-29 21:51:57 -0500

    Initial commit

---
+++ answers.md
@@ -0,0 +1,20 @@
+# CMPS 2200 Recitation 06
+## Answers
+
+**Name:**_________________________
+**Name:**_________________________
+
+
+Place all written answers from `recitation-07.md` here for easier grading.
+
+
+
+- **2)**
+
+- **3)**
+
+- **4)**
+
+- **6)**
+
+- **8)**
---
+++ main.py
@@ -0,0 +1,24 @@
+def fib_recursive(n, counts):
+    """
+    Return the nth Fibonacci number. 
+    counts is a list of n+1 elements, where counts[i] is incremented
+    each time fib_recursive(i, counts) is called.
+    """    
+    counts[n] += 1
+    ###TODO
+    pass
+    
+
+    
+def fib_top_down(n, fibs):
+    ###TODO
+    pass
+
+
+def fib_bottom_up(n):
+    ###TODO
+    pass
+
+
+
+
